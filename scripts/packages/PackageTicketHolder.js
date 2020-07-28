acc566563146b160eeee779646efe1a6ce35be52
jamesnitz <jamesanitz2@gmail.com>
2020-07-28 08:57:54 -0500

    foreach refactor

--- scripts/packages/PackageTicketHolder.js
+++ scripts/packages/PackageTicketHolder.js
@@ -3,11 +3,7 @@ const eventHub = document.querySelector("#state-fair")
 
 export const packageTicketHolder = () => {
     eventHub.addEventListener("packageTicketPurchased", customEvent => {
-      for (const target of contentTarget) {
-        target.innerHTML += `
-            <div class="person bigSpender">
-            </div>`
-      }
+      contentTarget.forEach(target => target.innerHTML += `<div class="person bigSpender"></div>`)
     })
 }
 
